e6488b74c1ef17250d92b460c44763e1e6012c50
chuma-beep <wisboynelson123@gmail.com>
2024-01-22 05:44:20 +0100

    merge two lists

---
+++ Algorithms practice/recursion.js
@@ -0,0 +1,23 @@
+//merge two sorted list using recursion.....
+//leet code challenge
+
+var mergeTwoLists = function(list1, list2) {
+    
+    if(!list1 || !list2)
+    return list1 || list2
+    
+     let head
+
+      if (list1.val < list2.val){
+          head = list1
+          head.next = mergeTwoLists(list1.next, list2)
+      }else{
+          head = list2
+          head.next = mergeTwoLists(list1, list2.next)
+      }
+      return head
+
+    {
+       
+    }
+};
